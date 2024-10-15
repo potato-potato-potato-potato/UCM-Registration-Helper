7728bfa5cff8cfe70562253cbba4ab4b4b1f9357
RyanMHsiao <109319635+RyanMHsiao@users.noreply.github.com>
2024-10-15 14:33:42 -0700

    Fix iframeScraper bug and replace consoleScraper

--- webScraper/iframeScraper.js
+++ webScraper/iframeScraper.js
@@ -7,7 +7,7 @@ async function getSCCList(pageOffset, sleep, txt_term = "202510", pageMaxSize = 
 	document.body.innerHTML = "";
 	// Short for registration helper iframe
 	const rhif = document.createElement("iframe");
-	rhif.setAttribute("href", `https://reg-prod.ec.ucmerced.edu/StudentRegistrationSsb/ssb/courseSearchResults/courseSearchResults?txt_term=${txt_term}&startDatepicker=&endDatepicker=&pageOffset=${pageOffset}&pageMaxSize=${pageMaxSize}&sortColumn=subjectDescription&sortDirection=asc`);
+	rhif.setAttribute("src", `https://reg-prod.ec.ucmerced.edu/StudentRegistrationSsb/ssb/courseSearchResults/courseSearchResults?txt_term=${txt_term}&startDatepicker=&endDatepicker=&pageOffset=${pageOffset}&pageMaxSize=${pageMaxSize}&sortColumn=subjectDescription&sortDirection=asc`);
 	// Hacky solution with closures
 	let rhifLoaded = false;
 	rhif.addEventListener("load", event => {
@@ -21,4 +21,33 @@ async function getSCCList(pageOffset, sleep, txt_term = "202510", pageMaxSize = 
 	const json = JSON.parse(rhif.contentDocument.querySelector("body>pre").innerText);
 	// If failing, test entry.subjectCode instead
 	return json.data.map(entry => `${entry.subject}${entry.courseNumber}`);
+}
+
+async function getCRNList(scc, pageOffset, sleep, txt_term = "202510", pageMaxSize = 10) {
+	document.body.innerHTML = "";
+	const rhif = document.createElement("iframe");
+	rhif.setAttribute("src", `https://reg-prod.ec.ucmerced.edu/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_subjectcoursecombo=${scc}&txt_term=${txt_term}&pageOffset=${pageOffset}&pageMaxSize=${pageMaxSize}&sortColumn=subjectDescription&sortDirection=asc`);
+	let rhifLoaded = false;
+	rhif.addEventListener("load", event => {
+		rhifLoaded = true;
+	});
+	// Required to make an iframe load
+	document.body.appendChild(rhif);
+	for (let i = 0; !rhifLoaded; ++i) {
+		await sleep(i);
+	}
+	const json = JSON.parse(rhif.contentDocument.querySelector("body>pre").innerText);
+	return json.data.map(entry => entry.courseReferenceNumber); 
+}
+
+// Weird sleep function that seems like it would be good for this usecase
+function weirdSleep(i) {
+	if (i <= 2) {
+		return null; // Let's just try not sleeping
+	} else if (i < 50) {
+		return new Promise(r => setTimeout(r, 100));
+	} else {
+		console.log(`Called sleep ${i}, which probably means something is broken`);
+		return new Promise(r => setTimeout(r, 5000));
+	}
 }
