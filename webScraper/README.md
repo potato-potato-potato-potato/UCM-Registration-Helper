23ea4a74bacad2c377a0f6ee764b836c28a3196e
RyanMHsiao <109319635+RyanMHsiao@users.noreply.github.com>
2024-10-16 12:07:36 -0700

    Implement function to scrape SCCs into a single JSON file

--- webScraper/README.md
+++ webScraper/README.md
@@ -1,13 +1,20 @@
 # webScraper
 
-## Purpose
-The purpose of the programs of this folder is to automatically collect course data.
-
 ## To-do
 - Decide upon a final format for course data fetched from CRN.
 - Decide how to handle npm dependencies.
+
+## Explanation
+The purpose of the programs of this folder is to automatically collect course data.
+This is done first by opening the school's website in a real browser, then running code from iframeScraper.js.
+That code generates data in JSON form, which can be saved to later be accessed locally.
+
+Two important abbreviations to understand are SCC and CRN.
+CRN, course reference number, is a 5 digit number that can be used to access course information through the website without even having a real browser open.
+SCC, subject course combo, is a string that is required to scrape for CRNs.
 
 ## Files
 - **iframeScraper.js** is a simple script that provides function to automatically get possible CRNs.
 It requires the school website to be open in a real browser, but it just gets JSON data instead of simulating clicks through pages.
 - **fetchCrn.mjs** is a simple script that takes a single CRN and returns an object that contains all information about the corresponding course.
+- **data/scc.json** is an object with SCC data that I scraped locally using iframeScraper.js.
--- webScraper/iframeScraper.js
+++ webScraper/iframeScraper.js
@@ -1,11 +1,29 @@
-// This file is intended for use on the page https://reg-prod.ec.ucmerced.edu/StudentRegistrationSsb/ssb/registration
-// Currently, it only provides some basic functions and does not implement the actual scraping.
+// This file is intended for use on the following page:
+// https://reg-prod.ec.ucmerced.edu/StudentRegistrationSsb/ssb/registration
 
+// For best results, first navigate to a state with a table of data first
 // Sometimes, this code just does not work and trying to create the iframe results in a 500 error
 // I believe this is just due to a cookie expiring, so close the window, reopen, and try again
 
+// Scrapes all SCC data into a single object
 // Short for get subject course combo list
-async function getSCCList(pageOffset, sleep, txt_term = "202510", pageMaxSize = 10) {
+async function getSCCList(txt_term = "202510", pageMaxSize = 50) {
+	const output = {};
+	for (let i = 0; ; i += 50) {
+		await sleepBetweenQueries(i);
+		const entries = await querySCC(i, sleepForIframe, txt_term, pageMaxSize);
+		if (entries.length === 0) {
+			break;
+		}
+		entries.forEach(entry => {
+			output[entry[0]] ??= [];
+			output[entry[0]].push(entry[1]);
+		});
+	}
+	return output;
+}
+
+async function querySCC(pageOffset, sleep, txt_term = "202510", pageMaxSize = 50) {
 	// Does not remove all HTML, but suffices
 	document.body.innerHTML = "";
 	// Short for registration helper iframe
@@ -23,10 +41,10 @@ async function getSCCList(pageOffset, sleep, txt_term = "202510", pageMaxSize = 
 	}
 	const json = JSON.parse(rhif.contentDocument.querySelector("body>pre").innerText);
 	// If failing, test entry.subjectCode instead
-	return json.data.map(entry => `${entry.subject}${entry.courseNumber}`);
+	return json.data.map(entry => [entry.subject, entry.courseNumber]);
 }
 
-async function getCRNList(scc, pageOffset, sleep, txt_term = "202510", pageMaxSize = 10) {
+async function getCRNList(scc, pageOffset, sleep, txt_term = "202510", pageMaxSize = 50) {
 	document.body.innerHTML = "";
 	const rhif = document.createElement("iframe");
 	rhif.setAttribute("src", `https://reg-prod.ec.ucmerced.edu/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_subjectcoursecombo=${scc}&txt_term=${txt_term}&pageOffset=${pageOffset}&pageMaxSize=${pageMaxSize}&sortColumn=subjectDescription&sortDirection=asc`);
@@ -44,7 +62,7 @@ async function getCRNList(scc, pageOffset, sleep, txt_term = "202510", pageMaxSi
 }
 
 // Weird sleep function that seems like it would be good for this usecase
-function weirdSleep(i) {
+function sleepForIframe(i) {
 	if (i <= 2) {
 		return null; // Let's just try not sleeping
 	} else if (i < 50) {
@@ -53,4 +71,8 @@ function weirdSleep(i) {
 		console.log(`Called sleep ${i}, which probably means something is broken`);
 		return new Promise(r => setTimeout(r, 5000));
 	}
+}
+
+function sleepBetweenQueries(_i) {
+	return new Promise(r => setTimeout(r, 1000));
 }
