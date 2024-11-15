2d4e4630884f6333e023caa901e0f1574c472d74
Seth <142448427+Flucoreo@users.noreply.github.com>
2024-11-15 00:49:07 -0800

    parsing json

--- ucmrh/src/pages/index.js
+++ ucmrh/src/pages/index.js
@@ -1,24 +1,34 @@
 import React from 'react'
-import PreferredDay from '../components/preferred-day'; 
+// import PreferredDay from '../components/preferred-day'; 
+import CenterClassCard from '../components/center-class-card'; 
+import Classes from "../components/pared.json"
+
 export const index = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-				<PreferredDay/>
-        <a
-          className="App-link"
-          href="https://reactjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <PreferredDay /> */}
+
+      <CenterClassCard classes={Classes}/>
+      
+      {/* <div className="App">
+         <header className="App-header">
+          
+           <p>
+             Edit <code>src/App.js</code> and save to reload.
+           </p>
+           <a
+             className="App-link"
+             href="https://reactjs.org/"
+             target="_blank"
+             rel="noopener noreferrer"
+           >
+             Learn React
+           </a>
+         </header>
+      </div> */}
+    </>
+    
   )
 }
 
