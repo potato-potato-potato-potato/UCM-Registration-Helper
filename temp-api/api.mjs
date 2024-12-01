f79f6a91d18a5f1e5a4b3c04444eec93538b96ae
RyanMHsiao <109319635+RyanMHsiao@users.noreply.github.com>
2024-11-30 23:19:36 -0800

    Add functions for meeting times and linked courses

--- temp-api/api.mjs
+++ temp-api/api.mjs
@@ -18,6 +18,27 @@ export class API {
 		const json = parseSpanEntries(html);
 		return `${json.Subject} ${json["Course Number"]} ${json["Section Number"]}`;
 	}
+	/**
+	 * @param {(string|number)} crn 5 digit identifier for a section
+	 */
+	async getMeetingTimes(crn) {
+		const textData = await querySsb("getFacultyMeetingTimes", crn, this.term);
+		const json = JSON.parse(textData);
+		return json.fmt[0].meetingTime; // Format not finalized
+	}
+	/**
+	 * @param {(string|number)} crn 5 digit identifier for a section
+	 */
+	async getLinkedSections(crn) {
+		const textData = await querySsb("getLinkedSections", crn, this.term);
+		const table = parseHtmlTable(textData);
+		let output = [];
+		// Iterate through all entries of type tbody
+		for (let i = 3; i < table.length; i += 3) {
+			output.push(table[i].data[0][3].substring(4, 9));
+		}
+		return output;
+	}
 }
 
 // Following functions are not exported
@@ -59,6 +80,29 @@ function parseSpanEntries(htmlString) {
 			result[currentProperty] = innerText;
 			currentProperty = null;
 		}
+	}
+	return result;
+}
+
+// Logic from algorithm/parseHtml.mjs
+const matchRowGroup = /<(thead|tbody)>[\s\S]*?<\/\1>/g;
+const matchTr = /<tr>[\s\S]*?<\/tr>/g;
+const matchTdTh = /<(td|th)[^>]*>[\s\S]*?<\/\1>/g;
+function parseHtmlTable(htmlString) {
+	let result = [];
+	const matches = htmlString.matchAll(matchRowGroup);
+	for (const match of matches) {
+		let rowGroup = { type: match[1], data: [] };
+		const rows = match[0].matchAll(matchTr);
+		for (const row of rows) {
+			rowGroup.data.push([...row[0].matchAll(matchTdTh)].map(x => {
+				return x[0];
+			}));
+		}
+		if (rowGroup.data.length === 0) {
+			rowGroup.data = { noTr: true, data: match[0].match(matchTdTh)[0] };
+		}
+		result.push(rowGroup);
 	}
 	return result;
 }
